6c7d36e7f7d72cd4bb5dc0ad2b0da8606bea60f6
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-01-09 12:55:44 +0900

    Jan 9th 2022 work. I'm on page 288 and working on real practice question 5-3 AppRouter component

---
+++ src/AppRouter.js
@@ -0,0 +1,78 @@
+/*============================================================
+* Jan 8th 2022.
+* 실습코드 5-3.AppRouter 컴포넌트 
+* Login 컴포넌트로 라우팅할 수 있는  src/AppRoute.js를 추가
+* AppRoute.js에 모든 라우팅 규칙을 작성한다.
+============================================================*/
+
+import React from "react";
+import "./index.css";
+import App from "./App";
+import Login from "./Login";
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // Jan 9th 2022, react-router-dom 버젼 6.2.1사유로 Switch가 적용이 안됨.
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+// Jan 9th 2022, below function and class were added by thomas lee
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            Thomasleeyou34, {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+class AppRouter extends React.Component {
+    render() {
+        return (
+            //====================================As-IS
+            // 실습코드 5-3 AppRouter.js에서 Switch 태그가 react-router-dom에 적용되지 않아 주석처리함.
+            // <div>
+            //     <Router>
+            //         <div>
+            //             <Switch>
+            //                 <Route path="/login">
+            //                     <Login />
+            //                 </Route>
+            //                 <Route path="/">
+            //                     <App />
+            //                 </Route>
+            //             </Switch>
+            //         </div>
+            //         <Box mt={5}>
+            //             <Copyright />
+            //         </Box>
+            //     </Router>
+            // </div>
+            //=======================================
+            //=======================================To-Be
+            //<Switch>태그가 <Routes로 변환됨> https://github.com/fsoftwareengineer/todo-application/discussions/29
+            <div>
+            <Router>
+                <div>
+                    <Routes>
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                        <Route path="/">
+                            <App />
+                        </Route>
+                    </Routes>
+                </div>
+                <Box mt={5}>
+                    <Copyright />
+                </Box>
+            </Router>
+            </div>
+        );
+    }
+}
+
+
+export default AppRouter;
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react'; // 리액트의 사용을 위해 import (예제 3-9)
 import ReactDOM from 'react-dom'; // 리액트 DOM의 사용을 위해
 import './index.css'; // css import
-import App from './App'; //App component import
+//import App from './App'; //App component import // 예제 3-9
+import AppRouter from './AppRouter'; //AppRouter component is imported (실습코드 5-4)
 import reportWebVitals from './reportWebVitals'; // 지금은 무시해도 됨
 
 ReactDOM.render( // ReactDOM이 내부의 컴포넌트들을 'root' 엘리멘트에 render함 // App Component 사용법
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//  <React.StrictMode> // Jan 9th, 실습코드 5-4를 위해 해당 index.js React부분 주석
+//    <App />
+//  </React.StrictMode>,
+
+/*=========================================
+* Jan 9th 2022, 실습코드 5-4 index.js 수정
+* 이제 경로에 따라 실행되는 컴포넌트가 다름으로, 
+* 그정보를 갖고 있는 AppRouter를 가장 먼저 렌더링 해야함.
+* index.js로 가서 맨 처음 렌더링 되는 컴포넌트가 AppRouter 컴포넌트가 되겠금 수정
+*
+==========================================*/
+    <React.StrictMode>
+      <AppRouter />
+    </React.StrictMode>,
+document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
